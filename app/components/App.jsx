67245a2e48677b2450eeaad3d65ae679b513bcec
Krystal Campioni <krystal.designer@gmail.com>
2016-12-25 21:11:23 -0200

    initial flux setup

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import uuid from 'uuid'
 import Notes from './Notes';
+import connect from '../libs/connect';
 
-
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
@@ -66,6 +66,7 @@ export default class App extends React.Component {
     const {notes} = this.state;
     return (
       <div>
+        {this.props.test}
         <button onClick={this.addNote}>+</button>
         <Notes
           notes={notes}
@@ -77,3 +78,7 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default connect(() => ({
+  test: 'test'
+}))(App)
